b10fddd1cd0df9f77004fcc5d41307ac905c6af9
Shehriyar Nadeem <sacrosanct_smk@hotmail.com>
2023-07-23 01:40:14 +0500

    Commit

--- front/common/ApiClient.js
+++ front/common/ApiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://coursemule-a65e519e93a4.herokuapp.com/api';
 const API_HEADERS = {
   'Content-Type': 'application/json'
 };
